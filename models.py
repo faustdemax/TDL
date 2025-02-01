1fe1a16c3c8b8fd6fa8f5c324af66d0c66853282
faustdemax <demax.faustine@gmail.com>
2025-02-01 16:37:33 +0100

    changes

--- models.py
+++ models.py
@@ -1,16 +1,13 @@
 import torch.nn as nn
 
-class FullyConnectedNetwork(nn.Module):
-    
+class SimpleFCN(nn.Module):
     def __init__(self, input_size, width, output_size):
-        super(FullyConnectedNetwork, self).__init__()
+        super(SimpleFCN, self).__init__()
         self.fc1 = nn.Linear(input_size, width)
-        self.fc2 = nn.Linear(width, width)
-        self.fc3 = nn.Linear(width, output_size)
-        self.activation = nn.ReLU()
+        self.fc2 = nn.Linear(width, output_size)
+        self.activation = nn.Softplus(beta = 5)
 
     def forward(self, x):
         x = self.activation(self.fc1(x))
-        x = self.activation(self.fc2(x))
-        x = self.fc3(x)
-        return x    
+        x = self.fc2(x)
+        return x
